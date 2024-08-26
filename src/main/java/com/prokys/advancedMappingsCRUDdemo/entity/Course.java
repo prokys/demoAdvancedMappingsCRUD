297f48137b9fe5f0132865cd2f0d25f2029d0a3d
Prokop Ferdan <prokes1@gmail.com>
2024-08-26 15:11:06 +0200

    created class Course with field annotations

---
+++ src/main/java/com/prokys/advancedMappingsCRUDdemo/entity/Course.java
@@ -0,0 +1,64 @@
+package com.prokys.advancedMappingsCRUDdemo.entity;
+
+import jakarta.persistence.*;
+
+@Table(name = "course")
+public class Course {
+
+    //define fields and annotate them
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    //define constructors
+
+    public Course() {
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    //define getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    //define toString()
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
